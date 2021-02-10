29765475677127a2138daab34d217e46ffa55cb9
HootDunk <58009556+HootDunk@users.noreply.github.com>
2021-02-09 19:56:22 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      <img src="https://images.squarespace-cdn.com/content/v1/5c8a78c9e5f7d15aab22c61c/1554844875675-NIF2A6775IC21BTEHCA1/ke17ZwdGBToddI8pDm48kObvEeIazJgnuf-7xVW_1Dd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1Udwqg1gDotxrgJm-lEh6bU1fjB8-XYfUSm2pjRBHT3jeZoPb5-ca2_Y2PNmhw3jNaQ/St_Louis_night_expblend.jpg" />
+      
     </div>
   );
 }
